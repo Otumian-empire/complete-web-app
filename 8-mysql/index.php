449466dcb5d053d5ddf4628c12e9e0b1960b4c8a
otumian-empire <popecan1000@gmail.com>
2021-01-11 09:05:07 +0000

    Simple signup for,

--- 8-mysql/index.php
+++ 8-mysql/index.php
@@ -27,29 +27,98 @@ if (mysqli_connect_error()) {
 // }
 
 // update
-$email = 'john123@gmail.com';
-$s_email = 'john@gmail.com';
+// $email = 'john123@gmail.com';
+// $s_email = 'john@gmail.com';
 
-$s_password = '1234563qgfdh';
+// $s_password = '1234563qgfdh';
 
-$query = "UPDATE `susers` SET `email` = '$email' WHERE email = '$s_email'";
-$result = mysqli_query($link, $query);
+// $query = "UPDATE `susers` SET `email` = '$email' WHERE email = '$s_email'";
+// $result = mysqli_query($link, $query);
 
 
-if ($result) {
-    echo $s_email . " update<br>";
-} else {
-    die(mysqli_connect_error());
-}
+// if ($result) {
+//     echo $s_email . " update<br>";
+// } else {
+//     die("Update Error<br>");
+// }
 
 
 // select one item from the table
-$query = "SELECT `id`, `email`, `password` FROM `susers`;";
+// $query = "SELECT `id`, `email`, `password` FROM `susers`;";
 
-$result = mysqli_query($link, $query);
+// $result = mysqli_query($link, $query);
 
-if ($result) {
-    $rows = mysqli_fetch_array($result);
+// if ($result) {
+//     $rows = mysqli_fetch_array($result);
 
-    echo "ID: " . $rows['id'] . ", Email: " . $rows['email'] . ", Password: " . $rows['password'] . "<br>";
+//     echo "ID: " . $rows['id'] . ", Email: " . $rows['email'] . ", Password: " . $rows['password'] . "<br>";
+// }
+
+
+// select all users
+// $query = "SELECT `id`, `email`, `password` FROM `susers`;";
+
+// if ($result = mysqli_query($link, $query)) {
+//     while ($rows = mysqli_fetch_array($result)) {
+//         echo "ID: " . $rows['id'] . ", Email: " . $rows['email'] . ", Password: " . $rows['password'] . "<br>";
+//     }
+// }
+
+
+// selecting with LIKE %-anything
+// mysqli_real_escape_string(str)
+
+if (!isset($_POST['email']) || !isset($_POST['password'])) {
+    echo ("Email and Password required");
+} else {
+
+    $email = mysqli_real_escape_string($link, $_POST['email']);
+    $password = mysqli_real_escape_string($link, $_POST['password']);
+
+    $select_query = "SELECT `id` FROM `susers` WHERE `email` =" . $email;
+    $result =  mysqli_query($link, $select_query);
+
+    echo $result . " select <br>";
+
+    if ($result) {
+        echo "Email already taken";
+    } else {
+        $insert_query = "INSERT INTO `susers`(`email`, `password`) VALUES(" . $email . "," . $password . ")";
+        $result =  mysqli_query($link, $insert_query);
+
+        if ($result) {
+            echo "Signup successful";
+        } else {
+            echo "Signup unsuccessful";
+        }
+    }
+
+    unset($_POST);
+    unset($email);
+    unset($password);
 }
+
+echo "<br>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <form action="" method="POST">
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" value="SignUp">
+        </form>
+    </div>
+</body>
+
+</html>
