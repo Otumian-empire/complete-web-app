73ab70f1ba3cff136b3f08eef146157ca2c88dcf
otumian-empire <popecan1000@gmail.com>
2021-01-11 03:14:20 +0000

    8-mysql/ - insert and update

--- 8-mysql/index.php
+++ 8-mysql/index.php
@@ -12,6 +12,38 @@ if (mysqli_connect_error()) {
     die("Connection error<br>");
 }
 
+// insert
+// $s_email = 'man1@gmail.com';
+// $s_password = '1234563qgfdh';
+
+// $query = "INSERT INTO `susers` (`email`, `password`) VALUES('$s_email',' $s_password')";
+// $result = mysqli_query($link, $query);
+
+
+// if ($result) {
+//     echo $s_email . " added to the users<br>";
+// } else {
+//     die(mysqli_connect_error());
+// }
+
+// update
+$email = 'john123@gmail.com';
+$s_email = 'john@gmail.com';
+
+$s_password = '1234563qgfdh';
+
+$query = "UPDATE `susers` SET `email` = '$email' WHERE email = '$s_email'";
+$result = mysqli_query($link, $query);
+
+
+if ($result) {
+    echo $s_email . " update<br>";
+} else {
+    die(mysqli_connect_error());
+}
+
+
+// select one item from the table
 $query = "SELECT `id`, `email`, `password` FROM `susers`;";
 
 $result = mysqli_query($link, $query);
